3efb0cd76a6d23c52833c608228df6e364700727
Renwarren <warren.chendjou@gmail.com>
2026-02-13 22:53:21 -0500

    Update portfolio

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ document.addEventListener("click",(e)=>{
 
 /* ----------------- About Tabs -------------- */
 const tabsContainer = document.querySelector(".about-tabs");
-aboutSection = document.querySelector(".about-section");
+const aboutSection = document.querySelector(".about-section");
 
 tabsContainer.addEventListener("click",(e) =>{
     if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
